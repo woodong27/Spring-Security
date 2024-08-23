0d727343d2b8e8eafec924735d911b14e5972342
woodong27 <cdw5548@gmail.com>
2024-08-23 23:49:55 +0900

    React App initial commit

---
+++ oauth2/src/main/java/com/example/oauth2/jwt/JwtFilter.java
@@ -0,0 +1,45 @@
+package com.example.oauth2.jwt;
+
+import com.example.oauth2.dto.UserDto;
+import com.example.oauth2.dto.oauth2.CustomOAuth2User;
+import com.example.oauth2.entity.User;
+import com.example.oauth2.repository.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.apache.logging.log4j.util.InternalException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Arrays;
+
+@RequiredArgsConstructor
+public class JwtFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String token = Arrays.stream(request.getCookies())
+                .filter(cookie -> cookie.getName().equals("Authorization"))
+                .findFirst()
+                .orElseThrow(() -> new InternalException("11"))
+                .getValue();
+
+        CustomOAuth2User oAuth2User = new CustomOAuth2User(UserDto.builder()
+                .username(jwtUtil.getUsername(token))
+                .role(jwtUtil.getRole(token))
+                .build());
+        Authentication authToken = new UsernamePasswordAuthenticationToken(oAuth2User, null, oAuth2User.getAuthorities());
+        SecurityContextHolder.getContext().setAuthentication(authToken);
+
+        filterChain.doFilter(request, response);
+    }
+}
