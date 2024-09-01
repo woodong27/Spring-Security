c2bce0fc33576a7cea777934505b603eb2c2ec13
woodong27 <cdw5548@gmail.com>
2024-09-01 23:47:07 +0900

    Main Page 추가

--- oauth2-login/src/page/LoginPage.jsx
+++ oauth2-login/src/page/LoginPage.jsx
@@ -1,25 +1,32 @@
-import React from 'react';
+import React from "react";
+import axios from "axios";
 
 const handleNaverLogin = () => {
-    window.location.href = 'http://localhost:8080/oauth2/authorization/naver'
-}
+  window.location.href = "http://localhost:8080/oauth2/authorization/naver";
+};
 
 const handleGoogleLogin = () => {
-    window.location.href = 'http://localhost:8080/oauth2/authorization/google'
-}
+  window.location.href = "http://localhost:8080/oauth2/authorization/google";
+};
+
+const handleMainPage = () => {
+  axios
+    .get("http://localhost:8080/api/main", { withCredentials: true })
+    .then((res) => {
+      alert(JSON.stringify(res.data));
+    })
+    .catch((e) => alert(e));
+};
 
 const LoginPage = () => {
-    return (
-        <div>
-            <h1>Social Login</h1>
-            <button onClick={handleNaverLogin}>
-                Naver
-            </button>
-            <button onClick={handleGoogleLogin}>
-                Google
-            </button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h1>Social Login</h1>
+      <button onClick={handleNaverLogin}>Naver</button>
+      <button onClick={handleGoogleLogin}>Google</button>
+      <button onClick={handleMainPage}>Main Page</button>
+    </div>
+  );
+};
 
 export default LoginPage;
